e4226563b361daabb1a3fde8e5f5e5841d46074a
emsihyo <emsihyo@gmail.com>
2018-04-25 20:14:50 +0800

    1.0.2

---
+++ RetriableAFNetworking watchOS Example Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  RetriableAFNetworking watchOS Example Extension
+//
+//  Created by emsihyo on 2018/4/25.
+//  Copyright © 2018年 emsihyo. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
